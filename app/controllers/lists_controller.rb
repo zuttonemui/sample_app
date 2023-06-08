5d281dc116c2a2a941fc7370a196134d1895c657
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-06-08 09:14:28 +0000

    [Finish]テストコード作成

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,45 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+  
+end
---
+++ db/migrate/20230601084854_create_lists.rb
@@ -0,0 +1,12 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+
+      t.string :title
+      t.string :body
+      t.integer :image_id
+      t.timestamps
+      
+    end
+  end
+end
